29e9fe2c2868bc011b301a9b20f5dc67ee7c5362
Nick9113 <52991508+Nick9113@users.noreply.github.com>
2019-12-02 00:14:56 +0700

    Journal
    Добавлена работа с консольным меню. 
    Исправлены ашибки в архитектуре 
    
    Имеется ошибка, при показе старосты

--- Program.cs
+++ Program.cs
@@ -8,7 +8,6 @@ namespace ConsoleApp2
 {
     class Program
     {
-
         struct Student {
             private String fullName;
             private String group;
@@ -65,6 +64,10 @@ namespace ConsoleApp2
             public void MakeManager() {
                 isManager = true;
             }
+
+            public void DowngradeManager() {
+                isManager = false;
+            }
            
             private void sendMessage(string error) {
                 Console.WriteLine("ERROR: " + error);
@@ -79,7 +82,7 @@ namespace ConsoleApp2
         }
 
         struct Group {
-            private List<Student> students;
+            private List<Student> students ;
             private string groupName;
 
             public Group(string name) {
@@ -99,11 +102,13 @@ namespace ConsoleApp2
                 return manager;
             }
 
+          
+
             public void AddStudent(string fullName) {
                 students.Add(new Student(fullName, groupName));
             }
 
-            public void ExpelStident(string fullName) {
+            public void ExpelStudent(string fullName) {
                 students.Remove(SearchStudent(fullName));
             }
 
@@ -114,7 +119,11 @@ namespace ConsoleApp2
                 }
             }
 
-            private Student SearchStudent(string fullName) {
+            public string GetGroupName() {
+                return groupName;
+            }
+
+            public Student SearchStudent(string fullName) {
                 Student findedStudent = new Student();
                 foreach (Student student in students) { 
                     if (student.GetName() == fullName) {
@@ -130,36 +139,53 @@ namespace ConsoleApp2
 
 
         }
-       
-        
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Введите нужный Вам номер пункта меню:");
-            string[] tokens = new string[4] {"Создать группу", "Назначить старосту", "Добавить студента", "Завершить работу"};
 
-            for (int i = 0; i < tokens.Length; i++) {
-                Console.WriteLine((i+1) + " - " +tokens[i]);
+        struct Menu {
+
+            private List<string> menuList;
+
+            public Menu(string menuItem) {
+                menuList = new List<string>();
+                AddMenuItem(menuItem);
             }
 
-            while (true) {
+            public void AddMenuItem(string menuItem) {
+                menuList.Add(menuItem);
+            }
+
+
+        
+        }
+
+        static List<Group> groups = new List<Group>();
+
+        static void Main(string[] args)
+        {
+
+            bool isWorking = true;
+
+            while (isWorking) {
+                OpenMenu();
+
                 string item = Console.ReadLine();
                 switch (item) {
                     case "1":
-                        // Создать группу
+                        ShowGroups();
                         break;
                     case "2":
-                        // Назначить стросту
+                        AddGroup();
                         break;
-
                     case "3":
-                        // Добавить студента
+                        OpenGroupMenu(OpenGroup());
+
                         break;
 
                     case "4":
-                        // Завершить работу
+                        Console.WriteLine("Работа с журналом завершена.");
+                        isWorking = false;
                         break;
                     default:
-                        // Вывести ошибку
+                        Console.WriteLine("ERROR: Введите номер меню!");
                         break;
              
                 
@@ -171,7 +197,137 @@ namespace ConsoleApp2
 
             Console.Read();
         }
+       
 
+        
+        static void OpenMenu() {
+            
+            
+            Console.WriteLine("Введите нужный Вам номер пункта меню:");
+
+            string[] tokens = new string[] {"Показать список групп" ,"Создать группу", "Открыть группу", "Завершить работу" };
+
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                Console.WriteLine((i + 1) + " - " + tokens[i]);
+            }
+        }
+
+        static Group OpenGroup() {
+            Console.WriteLine("Введите называние группы");
+            string name = Console.ReadLine();
+            Group findedGroup = new Group();
+            bool isFind = false;
+            foreach (Group group in groups) {
+                if (group.GetGroupName() == name)
+                {
+                    findedGroup = group;
+                    isFind = true;
+                }
+            }
+            if (!isFind) {
+                Console.WriteLine("ERROR: Данная группа не найдена");
+                OpenGroup();
+            }
+
+            return findedGroup;
+        }
+
+        static void OpenGroupMenu(Group group) {
+          
+
+            
+            bool isWorking = true;
+
+            while (isWorking) {
+                ShowGroupMenu();
+                string item = Console.ReadLine();
+                switch (item)
+                {
+                    case "1":
+                        AddStudent(group);
+                        break;
+                    case "2":
+                        ExpelStudent(group);
+                        break;
+                    case "3":
+                        ShowManager(group);
+                        break;
+                    case "4":
+                        MakeManager(ref group);
+                        break;
+                    case "5":
+                        Console.Write("Список группы ");
+                        group.ShowGroup();
+                        Console.WriteLine();
+
+                        break;
+                    case "6":
+                        Console.WriteLine("Возврат к главному меню");
+                        isWorking = false;
+                        break;
+                    default:
+                        Console.WriteLine("ERROR: Введите номер меню!");
+                        break;
+
+
+                }
+
+
+            }
+
+        }
+
+        static void ShowGroupMenu() {
+            Console.WriteLine("Введите нужный Вам номер пункта меню:");
+
+            string[] tokens = new string[] { "Добавить студента", "Отчислить сутдента", "Показать саросту", "Назначить старосту", "Посмотреть список группы", "Вернуться к главному меню" };
+
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                Console.WriteLine((i + 1) + " - " + tokens[i]);
+            }
+        }
+
+        static void ShowGroups() {
+            Console.WriteLine("Список групп:");
+            foreach (Group group in groups) {
+                Console.WriteLine(group.GetGroupName());
+            }
+            Console.WriteLine();
+        }
+
+        static void AddStudent(Group group) {
+            Console.WriteLine("Введите имя нового студента полностью");
+            string name = Console.ReadLine();
+            group.AddStudent(name);
+
+            Console.WriteLine("Студент " + name + " был успешно зачислен в группу " + group.GetGroupName());
+
+        }
+        static void ExpelStudent(Group group) {
+            Console.WriteLine("Введите полное имя студента, который будет отчислен");
+            string name = Console.ReadLine();
+            group.ExpelStudent(name);
+            Console.WriteLine("Студент " + name + " был успешно ОТЧИСЛЕН из группы " + group.GetGroupName());
+        }
+        static void MakeManager(ref Group group) {
+            Console.WriteLine("Введите имя нового старосты");
+            group.GetManager().DowngradeManager();
+            string name = Console.ReadLine();
+            group.SearchStudent(name).MakeManager();
+        }
+
+        static void ShowManager(Group group) {
+            Console.WriteLine("Староста группы " + group.GetGroupName() + " - " + group.GetManager().GetName());
+        }
+
+        static void AddGroup() {
+            Console.WriteLine("Введите название группы:");
+            string groupName = Console.ReadLine();
+            groups.Add(new Group(groupName));
+
+        }
 
         static int[] ReadArray(char din) {
 
